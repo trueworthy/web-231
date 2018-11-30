879426fb16f6d90b0ac31c25703013a248b5110e
Lea Trueworthy <ltrueworthy@my365.bellevue.edu>
2018-11-29 16:21:45 -0800

    primitive types

---
+++ week-1/trueworthy-primitive-types.js
@@ -0,0 +1,22 @@
+/* Using the provided starter code, create a JavaScript program that provides the following functionality / features. */
+var myname = "Lea Trueworthy";
+
+ console.log(myname)
+
+ var myfirstname = "Lea";
+ var mylastname = "Trueworthy";
+
+ console.log(myfirstname);
+ console.log(mylastname);
+
+ var myage = "25";
+ var mystreet = "Ryland";
+ var mycity = "Reno";
+ var mystate = "Nevada";
+ var myzip = "89502";
+
+ console.log(myage);
+ console.log(mystreet);
+ console.log(mycity);
+ console.log(mystate);
+ console.log(myzip);
