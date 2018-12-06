f28049201aa49189fbf682f33b69b179662c3ed8
Lea Trueworthy <ltrueworthy@my365.bellevue.edu>
2018-12-06 13:10:02 -0800

    assignment 2.3

---
+++ week-2/trueworthy-assignment-2.3.js
@@ -0,0 +1,24 @@
+var header=require('./trueworthy-header');
+
+
+/*  expected output:
+
+Lea Trueworthy
+Assignment 2.3
+12/6/18 
+
+*/
+
+console.log(header.display("Lea", "Trueworthy", "Assignment 2.3") 
+ )
+
+myName.Lea="Lea";
+
+// funtion  my name is inisde the whole function
+function myName() {
+    return myName.Lea;
+
+}
+
+//output    console.log(myName);*/
+console.log( "\n" +  "Hello " + myName() + " Trueworthy!");
